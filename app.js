be6305465bc6a5c2114b8d3958a679637249d022
Yossi Itzinger <yossi@alluresecurity.com>
2018-10-09 12:47:12 -0400

    remainder of CRUD added to app

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+
 
 //get rid of mongooses error
 
@@ -29,9 +32,12 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const port = 5000;
-
+//method override middleware
+app.use(methodOverride('_method'));;
 
 //index route
 
@@ -49,12 +55,97 @@ app.get('/about', (req, res) => {
     res.render('about')
 });
 
+//add ideas redirect
+
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+    .sort({date: 'desc'})
+    .then(ideas => {
+        res.render('ideas/index', {
+            ideas: ideas
+        });
+    })
+    
+});
+
 //add idea form
 
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add')
 });
 
+//edit idea form
+
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit', {
+            idea: idea
+        })
+    })
+    
+});
+
+
+//process form
+
+app.post('/ideas', (req, res) => {
+   let errors = [];
+   if(!req.body.title){
+       errors.push({text: 'please add a title'});
+   }
+   if(!req.body.details) {
+       errors.push({text: 'please add some details'});
+   }
+
+   if(errors.length > 0) {
+       res.render('ideas/add', {
+           errors: errors,
+           title: req.body.title,
+           details: req.body.details
+       });
+   } else{
+       const newUser = {
+           title: req.body.title,
+           details: req.body.details
+       }
+       new Idea(newUser)
+       .save()
+       .then(idea => {
+           res.redirect('/ideas');
+       })
+   }
+});
+
+//edit form process
+
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+        .then(idea => {
+            res.redirect('/ideas');
+        })
+    })
+});
+
+//delete ideas
+
+app.delete('/ideas/:id', (req, res) => {
+    Idea.deleteOne({_id: req.params.id })
+    .then(()=>{
+        res.redirect('/ideas');
+    })
+})
+
+const port = 5000;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
